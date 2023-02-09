9d4ff375ea2b966cd331fefbaedf8d506dc57c73
tlm <aconserver@hotmail.com>
2023-02-09 16:25:03 +0100

    2023-02-09

--- pages/discos/[id].js
+++ pages/discos/[id].js
@@ -19,27 +19,39 @@ import { Separator } from "../../comps/separator";
 import { getCurrentUser } from "../../utils/auth";
 
 export const getStaticProps = async ({ params }) => {
-  const { data } = await API.graphql({
-    query: getDisco,
-    variables: { id: params.id },
-    authMode: "AWS_IAM",
-  });
-  return { props: { disco: data.getDisco }, revalidate: 10 };
+  try {
+    const { data } = await API.graphql({
+      query: getDisco,
+      variables: { id: params.id },
+      authMode: "AWS_IAM",
+    })
+    return { props: { disco: data.getDisco }, revalidate: 10 }
+  }
+  catch (err) {
+    console.log(err);
+    return { props: {}, revalidate: 10 };
+  }
 }
 
 export const getStaticPaths = async () => {
-  const response = await API.graphql({
-    query: listDiscos,
-    authMode: "AWS_IAM",
-  });
-  const paths = response.data.listDiscos.items.map((s) => {
-    return {
-      params: {
-        id: s.id,
-      },
-    };
-  });
-  return { paths, fallback: false };
+  try {
+    const response = await API.graphql({
+      query: listDiscos,
+      authMode: "AWS_IAM",
+    });
+    const paths = response.data.listDiscos.items.map((s) => {
+      return {
+        params: {
+          id: s.id,
+        },
+      };
+    });
+    return { paths, fallback: false };
+  }
+  catch (err) {
+    console.log(err);
+    return { paths: [], fallback: false};
+  }
 }
 
 const Disco = ({ disco }) => {
