10278fa9b13d8908cbcd9dea0ff4c87e56c48ce0
tlm <aconserver@hotmail.com>
2023-02-16 11:33:47 +0100

    2023-02-16

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -11,6 +11,8 @@ function generateSiteMap(sites, discos, organisers) {
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
      <url>
        <loc>https://www.patrimonia.app</loc>
+       <lastmod>${new Date().toISOString().slice(0,10)}</lastmod>
+       <changefreq>daily</changefreq>
        <xhtml:link rel="alternate" hreflang="en" href="https://www.patrimonia.app/en"/>
        <xhtml:link rel="alternate" hreflang="es" href="https://www.patrimonia.app/es"/>
        <xhtml:link rel="alternate" hreflang="de" href="https://www.patrimonia.app/de"/>
@@ -18,6 +20,8 @@ function generateSiteMap(sites, discos, organisers) {
      </url>
      <url>
        <loc>https://www.patrimonia.app/sites</loc>
+       <lastmod>${new Date().toISOString().slice(0,10)}</lastmod>
+       <changefreq>daily</changefreq>
        <xhtml:link rel="alternate" hreflang="en" href="https://www.patrimonia.app/en/sites"/>
        <xhtml:link rel="alternate" hreflang="es" href="https://www.patrimonia.app/es/sites"/>
        <xhtml:link rel="alternate" hreflang="de" href="https://www.patrimonia.app/de/sites"/>
@@ -27,7 +31,9 @@ function generateSiteMap(sites, discos, organisers) {
        .map((x) => {
          return `
        <url>
-           <loc>${`${URL}/sites/${x}`}</loc>
+           <loc>${`${URL}/sites/${x.id}`}</loc>
+           <lastmod>${x.updatedAt.slice(0,10)}</lastmod> 
+           <changefreq>weekly</changefreq>
            <xhtml:link rel="alternate" hreflang="en" href="${`${URL}/en/sites/${x}`}"/>
            <xhtml:link rel="alternate" hreflang="es" href="${`${URL}/es/sites/${x}`}"/>
            <xhtml:link rel="alternate" hreflang="de" href="${`${URL}/de/sites/${x}`}"/>
@@ -41,7 +47,9 @@ function generateSiteMap(sites, discos, organisers) {
         .map((x) => {
           return `
       <url>
-          <loc>${`${URL}/discos/${x}`}</loc>
+          <loc>${`${URL}/discos/${x.id}`}</loc>
+          <lastmod>${x.updatedAt.slice(0,10)}</lastmod> 
+          <changefreq>monthly</changefreq>
           <xhtml:link rel="alternate" hreflang="en" href="${`${URL}/en/discos/${x}`}"/>
           <xhtml:link rel="alternate" hreflang="es" href="${`${URL}/es/discos/${x}`}"/>
           <xhtml:link rel="alternate" hreflang="de" href="${`${URL}/de/discos/${x}`}"/>
@@ -55,7 +63,9 @@ function generateSiteMap(sites, discos, organisers) {
         .map((x) => {
           return `
         <url>
-            <loc>${`${URL}/organisers/${x}`}</loc>
+            <loc>${`${URL}/organisers/${x.id}`}</loc>
+            <lastmod>${x.updatedAt.slice(0,10)}</lastmod> 
+            <changefreq>monthly</changefreq>
             <xhtml:link rel="alternate" hreflang="en" href="${`${URL}/en/organisers/${x}`}"/>
             <xhtml:link rel="alternate" hreflang="es" href="${`${URL}/es/organisers/${x}`}"/>
             <xhtml:link rel="alternate" hreflang="de" href="${`${URL}/de/organisers/${x}`}"/>
@@ -76,25 +86,26 @@ export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
 
   const req = await API.graphql({ query: listSites, authMode: "AWS_IAM" });
-  const sites = req.data.listSites.items.map((x) => x.id);
+  const sites = req.data.listSites.items;
 
   const req1 = await API.graphql({
     query: listDiscos,
     variables: { filter: { dateEnd: { gt: today } } },
     authMode: "AWS_IAM",
   });
-  const discos = req1.data.listDiscos.items.map((x) => x.id);
+  const discos = req1.data.listDiscos.items;
 
   const req2 = await API.graphql({
     query: listOrganisers,
     authMode: "AWS_IAM",
   });
-  const organisers = req2.data.listOrganisers.items.map((x) => x.id);
+  const organisers = req2.data.listOrganisers.items;
 
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(sites, discos, organisers);
 
-  res.setHeader("Content-Type", "text/xml");
+  //res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Content-Type", "application/xml")
   // we send the XML to the browser
   res.write(sitemap);
   res.end();
